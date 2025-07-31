cee1da5b8c6e0755b4cac36a6dd2d0889560a56b
LMacrini <liomacrini@gmail.com>
2025-07-31 12:32:47 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -3,27 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      formatter = pkgs.alejandra;
+  }: let
+    forAllSystems = f: builtins.mapAttrs f nixpkgs.legacyPackages;
+  in {
+    formatter = forAllSystems (system: pkgs: pkgs.alejandra);
 
-      devShells.default = pkgs.mkShell {
+    devShells = forAllSystems (system: pkgs: {
+      default = pkgs.mkShellNoCC {
         packages = with pkgs; [
           cloudflared
           nodejs
+          nodePackages.prettier
           superhtml
           typescript-language-server
           yarn
         ];
       };
     });
+  };
 }
